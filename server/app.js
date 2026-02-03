3f8f3c4c63e1142fe93029ffdb44ec862a5e795d
RamAsheesh Chauhan <rch231198@gmail.com>
2026-02-03 21:23:15 +0530

    cors fixing

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", process.env.CLIENT_URL],
   credentials: true
 }));
 
@@ -23,7 +23,7 @@ app.use(cors({
 app.use(logger);
 app.use(cookieParser());
 // app.use(cors({
-//   origin: process.env.NODE_ENV === 'production' 
+//   origin: process.env.NODE_ENV === 'development' 
     // ? process.env.CLIENT_URL 
 //     : 'http://localhost:5173',
 //   credentials: true
