41ad3109659b53eabad87a1290503e5c55928a50
RamAsheesh Chauhan <43158746+Ramasheesh@users.noreply.github.com>
2026-02-10 08:49:35 +0530

    Fix CORS configuration for development environmentport changed

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 // connectDB();
 
 app.use(cors({
-  origin: [process.env.NODE_ENV === 'development',"http://localhost:5173","https://skent.netlify.app/"], // Allow both local and deployed frontend
+  origin: ["http://localhost:5173","https://skent.netlify.app/"], // Allow both local and deployed frontend
   credentials: true
 }));
 
