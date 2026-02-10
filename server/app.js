fffd177494c45db801528a3e545621bae7bfedd9
RamAsheesh Chauhan <rch231198@gmail.com>
2026-02-10 22:36:05 +0530

    fcvb

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 // connectDB();
 
 app.use(cors({
-  origin: ["http://localhost:5173", process.env.CLIENT_URL], // Allow both local and deployed frontend
+  origin: process.env.CLIENT_URL || "http://localhost:5173", // Allow both local and deployed frontend
   credentials: true
 }));
  
