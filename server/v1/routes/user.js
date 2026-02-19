afa97308afa91bd40d12de7e291305fc4e8a14fd
RamAsheesh Chauhan <rch231198@gmail.com>
2026-02-19 21:58:15 +0530

    new code uploaded

--- server/v1/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userController = require('../controllers/user');
-
-const router = express.Router();
-
-// Onboarding route
-router.post('/onboarding', userController.onboarding);
-
-module.exports = router;
