a6b1646e43f668f31a0ec8d8b7ec9d7f0c6e60e0
RamAsheesh Chauhan <rch231198@gmail.com>
2026-02-20 07:42:39 +0530

    ddddd

--- app.js
+++ app.js
@@ -15,20 +15,19 @@ const port = process.env.PORT || 3000;
 // Connect to database
 // connectDB();
 
-app.use(cors({
-  origin: [
-    // "http://localhost:5173",
-    "https://sk-ent.netlify.app"
-  ],
-  credentials: true
-}));
-
- 
-// Middleware stack - FIXED CORS
+// Middleware stack
 app.use(logger);
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://sk-ent.netlify.app"
+  ],
+  credentials: true
+}));
 
 // Security middleware
 app.use(require('helmet')());
