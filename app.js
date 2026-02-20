a15b6b28d60de580e24380b70c0db4eecc648423
RamAsheesh Chauhan <43158746+Ramasheesh@users.noreply.github.com>
2026-02-20 07:09:56 +0530

    Update Netlify app URL in CORS configuration

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 3000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://skent.netlify.app"
+    "https://sk-ent.netlify.app"
   ],
   credentials: true
 }));
